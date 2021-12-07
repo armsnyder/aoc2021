b79787c8f29cee50036260bb0ebf6007b63be8cb
Adam Snyder <asnyder@ea.com>
2021-12-07 02:27:55 -0800

    Day 7 improve runtime by removing sort

--- rust/d07/src/main.rs
+++ rust/d07/src/main.rs
@@ -9,30 +9,27 @@ fn main() {
 }
 
 fn part1<R: BufRead>(reader: R) -> String {
-    let positions = parse_positions_sorted(reader);
-    let median = positions[positions.len() / 2];
-    get_total_linear_move_cost(&positions, median).to_string()
+    solve(reader, get_total_linear_move_cost)
 }
 
 fn part2<R: BufRead>(reader: R) -> String {
-    let positions = parse_positions_sorted(reader);
-    descend_gradient(&positions, get_total_triangular_move_cost).to_string()
+    solve(reader, get_total_triangular_move_cost)
 }
 
-fn parse_positions_sorted<R: BufRead>(reader: R) -> Vec<i32> {
-    let mut positions = reader
+fn solve<R: BufRead>(reader: R, cost_fn: impl Fn(&Vec<i32>, i32) -> i32) -> String {
+    descend_gradient(&parse_input(reader), cost_fn).to_string()
+}
+
+fn parse_input<R: BufRead>(reader: R) -> Vec<i32> {
+    reader
         .lines()
         .next()
         .unwrap()
         .unwrap()
         .split(",")
-        .map(str::parse::<i32>)
+        .map(str::parse)
         .map(Result::unwrap)
-        .collect::<Vec<i32>>();
-
-    positions.sort();
-
-    positions
+        .collect::<Vec<i32>>()
 }
 
 fn get_total_linear_move_cost(positions: &Vec<i32>, target_pos: i32) -> i32 {
@@ -48,14 +45,14 @@ fn get_total_triangular_move_cost(positions: &Vec<i32>, target_pos: i32) -> i32 
     })
 }
 
-fn descend_gradient(v: &Vec<i32>, f: impl Fn(&Vec<i32>, i32) -> i32) -> i32 {
+fn descend_gradient(v: &Vec<i32>, cost_fn: impl Fn(&Vec<i32>, i32) -> i32) -> i32 {
     let mut start = 0;
-    let mut end = v[v.len() - 1];
+    let mut end = *v.iter().max().unwrap();
     loop {
         let search = start + (end - start) / 2;
-        let cost = f(&v, search);
-        let next_cost = f(&v, search + 1);
-        let last_cost = f(&v, search - 1);
+        let cost = cost_fn(&v, search);
+        let next_cost = cost_fn(&v, search + 1);
+        let last_cost = cost_fn(&v, search - 1);
         if cost <= last_cost && cost <= next_cost {
             return cost;
         }
