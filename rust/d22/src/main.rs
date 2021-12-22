b407c11b961b15776edffd88bcd24b55a6bd1515
Adam Snyder <armsnyder@gmail.com>
2021-12-22 02:17:36 -0800

    Day 22 optimization

--- rust/d22/src/main.rs
+++ rust/d22/src/main.rs
@@ -3,6 +3,8 @@
 use std::cmp::{max, min};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::iter::Sum;
+use std::ops::Sub;
 
 fn main() {
     println!("{}", part1(read_input()));
@@ -10,74 +12,91 @@ fn main() {
 }
 
 fn part1<R: BufRead>(reader: R) -> String {
-    let initialization_volume = Volume {
-        0: Range(-50, 51),
-        1: Range(-50, 51),
-        2: Range(-50, 51),
-    };
+    let initialization_volume = Volume(
+        Range(-50, 51),
+        Range(-50, 51),
+        Range(-50, 51),
+    );
 
-    let mut reactor = Reactor::default();
+    let steps = parse_input(reader).into_iter()
+        .filter(|step| initialization_volume.contains(&step.volume))
+        .collect();
 
-    for step in parse_input(reader) {
-        if !initialization_volume.contains(&step.volume) {
-            continue;
-        }
-
-        if step.on {
-            reactor.add(step.volume);
-        } else {
-            reactor.sub(step.volume);
-        }
-    }
-
-    reactor.count().to_string()
+    count_cubes_after_steps(steps).to_string()
 }
 
 fn part2<R: BufRead>(reader: R) -> String {
-    let mut reactor = Reactor::default();
+    let steps = parse_input(reader);
+    count_cubes_after_steps(steps).to_string()
+}
 
-    for step in parse_input(reader) {
+fn count_cubes_after_steps(steps: Vec<Step>) -> usize {
+    let mut processed_steps = Vec::new();
+
+    for step in steps {
+        processed_steps.append(&mut processed_steps.iter()
+            .filter_map(|processed| *processed - step)
+            .collect());
+
         if step.on {
-            reactor.add(step.volume);
-        } else {
-            reactor.sub(step.volume);
+            processed_steps.push(step);
         }
     }
 
-    reactor.count().to_string()
+    processed_steps.iter().sum()
 }
 
 fn parse_input<R: BufRead>(reader: R) -> Vec<Step> {
     reader.lines()
         .map(Result::unwrap)
         .map(|s: String| {
-            let mut split = s.splitn(2, " ");
-            let mut step = Step::default();
-            step.on = split.next().unwrap() == "on";
-            let mut split = split.next().unwrap().splitn(3, ",");
-            step.volume.0 = parse_range(split.next().unwrap());
-            step.volume.1 = parse_range(split.next().unwrap());
-            step.volume.2 = parse_range(split.next().unwrap());
-            step
+            let (on, rest) = s.split_once(' ').unwrap();
+            let ranges = rest.splitn(3, ',')
+                .map(|s| {
+                    let (start, end) = s[2..].split_once("..").unwrap();
+                    Range(start.parse().unwrap(), end.parse::<i32>().unwrap() + 1)
+                })
+                .collect::<Vec<Range>>();
+            Step { on: on == "on", volume: Volume(ranges[0], ranges[1], ranges[2]) }
         })
         .collect()
 }
 
-fn parse_range(s: &str) -> Range {
-    let s = s.splitn(2, "=").nth(1).unwrap();
-    let mut split = s.splitn(2, "..");
-    let i = split.next().unwrap().parse::<i32>().unwrap();
-    let j = split.next().unwrap().parse::<i32>().unwrap() + 1;
-    Range(i, j)
-}
-
-#[derive(Default)]
+#[derive(Copy, Clone)]
 struct Step {
     volume: Volume,
     on: bool,
 }
 
-#[derive(Default, Copy, Clone)]
+impl Sub for Step {
+    type Output = Option<Step>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let volume = self.volume - rhs.volume;
+        if volume.size() == 0 {
+            None
+        } else {
+            Some(Step {
+                volume,
+                on: if self.on == rhs.on { !rhs.on } else { rhs.on },
+            })
+        }
+    }
+}
+
+impl<'a> Sum<&'a Step> for usize {
+    fn sum<I: Iterator<Item=&'a Step>>(iter: I) -> Self {
+        iter.fold(0, |acc, step| {
+            if step.on {
+                acc + step.volume.size()
+            } else {
+                acc - step.volume.size()
+            }
+        })
+    }
+}
+
+#[derive(Copy, Clone)]
 struct Volume(Range, Range, Range);
 
 impl Volume {
@@ -85,67 +104,41 @@ impl Volume {
         self.0.contains(&other.0) && self.1.contains(&other.1) && self.2.contains(&other.2)
     }
 
-    fn sub(self, other: &Self) -> Vec<Self> {
-        self.0.trisect(&other.0).into_iter().enumerate()
-            .filter(|(_, i_range)| i_range.len() > 0)
-            .flat_map(|(i, i_range)| {
-                self.1.trisect(&other.1).into_iter().enumerate()
-                    .filter(|(_, j_range)| j_range.len() > 0)
-                    .flat_map(move |(j, j_range)| {
-                        self.2.trisect(&other.2).into_iter().enumerate()
-                            .filter(|(_, k_range)| k_range.len() > 0)
-                            .filter(move |(k, _)| !(i == 1 && j == 1 && *k == 1))
-                            .map(move |(_, k_range)| Volume(i_range, j_range, k_range))
-                    })
-            })
-            .collect()
-    }
-
-    fn volume(&self) -> usize {
+    fn size(&self) -> usize {
         self.0.len() * self.1.len() * self.2.len()
     }
 }
 
-#[derive(Default, Copy, Clone)]
+impl Sub for Volume {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Volume(
+            self.0 - rhs.0,
+            self.1 - rhs.1,
+            self.2 - rhs.2,
+        )
+    }
+}
+
+#[derive(Copy, Clone)]
 struct Range(i32, i32);
+
+impl Sub for Range {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Range(min(self.1, max(self.0, rhs.0)), max(self.0, min(self.1, rhs.1)))
+    }
+}
 
 impl Range {
     fn contains(&self, other: &Self) -> bool {
         other.0 >= self.0 && other.1 <= self.1
     }
 
-    fn trisect(self, other: &Self) -> [Range; 3] {
-        let a = min(self.1, max(self.0, other.0));
-        let b = max(self.0, min(self.1, other.1));
-        [
-            Range(self.0, a),
-            Range(a, b),
-            Range(b, self.1),
-        ]
-    }
-
     fn len(&self) -> usize {
         (self.1 - self.0) as usize
-    }
-}
-
-#[derive(Default)]
-struct Reactor(Vec<Volume>);
-
-impl Reactor {
-    fn add(&mut self, volume: Volume) {
-        if !self.0.iter().any(|v| v.contains(&volume)) {
-            self.sub(volume);
-            self.0.push(volume);
-        }
-    }
-
-    fn sub(&mut self, volume: Volume) {
-        self.0 = self.0.iter().flat_map(|v| v.sub(&volume)).collect();
-    }
-
-    fn count(&self) -> usize {
-        self.0.iter().map(Volume::volume).sum()
     }
 }
 
@@ -193,10 +186,10 @@ mod tests {
         b.iter(|| part1(BufReader::new(input)))
     }
 
-    // #[bench]
-    // fn bench_part2(b: &mut Bencher) {
-    //     let input = fs::read_to_string("input.txt").unwrap();
-    //     let input = input.as_bytes();
-    //     b.iter(|| part2(BufReader::new(input)))
-    // }
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let input = fs::read_to_string("input.txt").unwrap();
+        let input = input.as_bytes();
+        b.iter(|| part2(BufReader::new(input)))
+    }
 }
